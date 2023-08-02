37c044a1b1dba349f95c33d98c520c617d02ff83
HoneyVanity <105306067+HoneyVanity@users.noreply.github.com>
2023-08-02 20:31:14 +0300

    initial_1

--- src/main/java/ru/practicum/shareit/item/ItemJpaRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemJpaRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.util.List;
@@ -10,12 +11,12 @@ import java.util.List;
 public interface ItemJpaRepository extends JpaRepository<Item, Long> {
     List<Item> findAllByOwnerId(Long ownerId, Pageable pageable);
 
-    @Query("select i " +
+    @Query(value = "select i " +
             "from Item i " +
-            "where (upper(i.name) like upper(concat('%', ?1, '%')) " +
-            "or upper(i.description) like upper(concat('%', ?1, '%'))) " +
+            "where (upper(i.name)) like upper(concat('%', :text, '%')) " +
+            "or upper(i.description) like upper(concat('%', :text, '%')) " +
             "and i.available is true")
-    List<Item> findAllByText(String text, Pageable pageable);
+    List<Item> findAllByText(@Param("text") String text, Pageable pageable);
 
     List<Item> findAllByRequestId(long requestId);
 
